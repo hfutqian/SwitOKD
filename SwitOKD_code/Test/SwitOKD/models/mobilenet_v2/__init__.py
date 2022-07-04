6fc8ad35e0de2e283b1247663532cb316e5404b3
hfutqian <84172730+hfutqian@users.noreply.github.com>
2022-07-04 11:12:02 +0800

    Add files via upload

---
+++ SwitOKD_code/Test/SwitOKD/models/mobilenet_v2/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/6/8 上午9:36
+@file: __init__.py.py
+@author: zj
+@description: 
+"""
---
+++ SwitOKD_code/Test/SwitOKD/models/mobilenet_v2/inverted_residual.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/6/8 下午4:18
+@file: inverted_residual.py
+@author: zj
+@description: 
+"""
+
+import torch.nn as nn
+
+from .conv_bn_relu import ConvBNReLU
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self, inp, oup, stride, expand_ratio):
+        super(InvertedResidual, self).__init__()
+        self.stride = stride
+        assert stride in [1, 2]
+
+        hidden_dim = int(round(inp * expand_ratio))
+        self.use_res_connect = self.stride == 1 and inp == oup
+
+        layers = []
+        if expand_ratio != 1:
+            # pw
+            layers.append(ConvBNReLU(inp, hidden_dim, kernel_size=1))
+        layers.extend([
+            # dw
+            ConvBNReLU(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim),
+            # pw-linear
+            nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(oup),
+        ])
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
